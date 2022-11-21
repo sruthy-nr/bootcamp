f88482920d4e168eae735fedffcbdc0f76fc699a
sruthy-nr <sruthyn.@nestgroup.net>
2022-11-21 10:22:37 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int c;
+        Scanner sc=new Scanner(System.in);
+        do {
+            System.out.println("MENU\n1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION");
+            System.out.println("ENTER YOUR CHOICE:");
+            c=sc.nextInt();
+        }while(c!=4);
     }
 }
